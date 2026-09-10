e1dcfb68fc6ffe73823f787fd5aed11e3a1791e3
DrifterAldvvyn <maestroKahlazar@proton.me>
2026-09-10 14:35:49 +0800

    Upadated webserver.sh to use to correct directory and parameters. \n Included log output.

--- webserver.sh
+++ webserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source .venv/bin/activate
-.venv/bin/gunicorn --workers 3 --bind unix:$HOME/mywork/EDDI_Chatbot/webhook.sock -m 007 src.app:app
+sudo .venv/bin/gunicorn --workers 3 --bind unix:/srv/www/twsigntube.org/EDDI_Chatbot/webhook.sock -m 007 --user $USER --group www-data src.app:app > webserver.log 2>&1 &

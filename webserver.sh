e9273ec2fd13e607d850c4cc76cbbd8110e37e11
DrifterAldvvyn <maestroKahlazar@proton.me>
2026-09-10 23:02:35 +0800

    # Fix Shell Script - Added `sudo -v` to validate first before starting the bg process. - Use `>>` to append to log file instead of overwriting it.

--- webserver.sh
+++ webserver.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 source .venv/bin/activate
-sudo .venv/bin/gunicorn --workers 3 --bind unix:/srv/www/twsigntube.org/EDDI_Chatbot/webhook.sock -m 007 --user $USER --group www-data src.app:app > webserver.log 2>&1 &
+sudo -v
+sudo .venv/bin/gunicorn --workers 3 --bind unix:/srv/www/twsigntube.org/EDDI_Chatbot/webhook.sock -m 007 --user $USER --group www-data src.app:app >> webserver.log 2>&1 &
